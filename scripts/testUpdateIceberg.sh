aae7b2a53d1bd4ae1c9cbf45602c9a41b13f8eb1
Martín Dias <tinchodias@gmail.com>
2020-07-30 13:28:11 -0400

    Scripts: Refactoring + add --dev option

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -2,5 +2,17 @@
 set -x
 set -e
 
-wget -O - get.pharo.org/90+vm | bash
-./pharo Pharo.image st --save --quit scripts/updateIceberg.st 
+if [[ $* == *--dev* ]]
+then
+    BASELINE_GROUPS=development
+else
+    BASELINE_GROUPS=default
+fi
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR="$(dirname "$SCRIPTS_DIR")"
+
+wget -O - get.pharo.org/64/90+vm | bash
+./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/preLoading.st"
+./pharo Pharo.image metacello install "tonel://$REPO_DIR" BaselineOfIceberg --groups=$BASELINE_GROUPS
+./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/postLoading.st"
