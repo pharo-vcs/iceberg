5b2aa86aff2cc8d4e8e00ce326f0fd48b8470f9d
Martín Dias <tinchodias@gmail.com>
2021-01-30 01:54:56 -0300

    update script: add support for MINGW*
    (Cherrypicked from rita2 branch)

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -9,10 +9,18 @@ else
     BASELINE_GROUPS=default
 fi
 
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+case "$(uname -s)" in
+   MINGW*)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -W )"
+     ;;
+   *)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+     ;;
+esac
+
 REPO_DIR="$(dirname "$SCRIPTS_DIR")"
 
 wget -O - get.pharo.org/64/90+vm | bash
-./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/preLoading.st"
+./pharo Pharo.image st --save --quit "$SCRIPTS_DIR/preLoading.st"
 ./pharo Pharo.image metacello install "tonel://$REPO_DIR" BaselineOfIceberg --groups=$BASELINE_GROUPS
-./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/postLoading.st"
+./pharo Pharo.image st --save --quit "$SCRIPTS_DIR/postLoading.st"
