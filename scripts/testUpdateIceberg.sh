a366ea6224e9c7ec9b02260231cca7a2b9e6f5dc
Martín Dias <tinchodias@gmail.com>
2021-10-22 13:01:48 -0300

    CI: use Pharo 10 (still keep P9 in ubuntu+macos)

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -20,7 +20,7 @@ esac
 
 REPO_DIR="$(dirname "$SCRIPTS_DIR")"
 
-wget -O - get.pharo.org/64/90+vm | bash
+wget -O - get.pharo.org/64/100+vm | bash
 ./pharo Pharo.image st --save --quit "$SCRIPTS_DIR/preLoading.st"
 ./pharo Pharo.image metacello install "tonel://$REPO_DIR" BaselineOfIceberg --groups=$BASELINE_GROUPS
 ./pharo Pharo.image st --save --quit "$SCRIPTS_DIR/postLoading.st"
